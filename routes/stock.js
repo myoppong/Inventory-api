ef6032eb01033171e6acac51febcce585a5cdb80
myoppong <michaeloppong03@gmail.com>
2025-05-25 23:39:28 +0000

    loc

--- routes/stock.js
+++ routes/stock.js
@@ -2,8 +2,9 @@
 import { Router } from 'express';
 
 import { createInventoryTransaction, listInventoryTransactions } from '../controller/stock.js';
+import { isAuthenticated } from '../middlewares/auth.js';
 
 const stockRouter = Router();
-stockRouter.post('/inventory', createInventoryTransaction); 
-stockRouter.get('/inventory', listInventoryTransactions);       // POST /api/stock
+stockRouter.post('/inventory',isAuthenticated, createInventoryTransaction); 
+stockRouter.get('/inventory',isAuthenticated, listInventoryTransactions);       // POST /api/stock
 export default stockRouter;
