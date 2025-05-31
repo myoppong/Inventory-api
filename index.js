61c04f7e6197227d4e93a8284abc7a0b3f8534f7
myoppong <michaeloppong03@gmail.com>
2025-05-31 16:33:15 +0000

    cors removed

--- index.js
+++ index.js
@@ -16,10 +16,12 @@ await mongoose.connect(process.env.MONGO_URI)
 const app = express();
 app.use(express.json());
 
-app.use(cors({
-  origin: 'http://localhost:3000', 
-  credentials: true,               
-}));
+
+// ({
+//   origin: 'http://localhost:3000', 
+//   credentials: true,               
+// })
+app.use(cors);
 app.use(userRouter);
 app.use(productRouter)
 app.use(categoryRouter)
